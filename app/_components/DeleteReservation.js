174c3c20de4b03f6f02089ab51b74c46ee89e2a0
Damian Roiz <damianx989@gmail.com>
2024-12-02 08:55:58 -0500

    added transition on delete reservation

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -13,7 +13,10 @@ function DeleteReservation({ bookingId, onDelete }) {
     }
   }
   return (
-    <button className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900">
+    <button
+      className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+      onClick={handleDelete}
+    >
       {!isPending ? (
         <>
           <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -9,28 +9,28 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
-  callbacks: {
-    authorized({ auth, request }) {
-      return !!auth?.user;
-    },
-    async signIn({ user, account, profile }) {
-      try {
-        const existingGuest = await getGuest(user.email);
+//   callbacks: {
+//     authorized({ auth, request }) {
+//       return !!auth?.user;
+//     },
+//     async signIn({ user, account, profile }) {
+//       try {
+//         const existingGuest = await getGuest(user.email);
 
-        if (!existingGuest)
-          await createGuest({ email: user.email, fullName: user.name });
+//         // if (!existingGuest)
+//         //   await createGuest({ email: user.email, fullName: user.name });
 
-        return true;
-      } catch {
-        return false;
-      }
-    },
-    async session({ session, user }) {
-      const guest = await getGuest(session.user.email);
-      session.user.guestId = guest.id;
-      return session;
-    },
-  },
+//         return true;
+//       } catch {
+//         return false;
+//       }
+//     },
+//     async session({ session, user }) {
+//       const guest = await getGuest(session.user.email);
+//       session.user.guestId = guest.id;
+//       return session;
+//     },
+//   },
   pages: {
     signIn: '/login',
   },
